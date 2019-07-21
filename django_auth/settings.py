82c6a3c4907f40968d858b3625e5d616f1072588
toderas <toaderchirila21@gmail.com>
2019-07-21 23:56:01 +0000

    /

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'django_forms_bootstrap',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+    ]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
